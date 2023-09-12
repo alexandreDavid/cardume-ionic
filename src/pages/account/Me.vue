64ff159e5831909fcc1b9e02a9cf89c2377d50fa
Alexandre David <alexandre.David42@gmail.com>
2023-09-12 14:51:15 +0200

    feat: add delete button

--- src/pages/account/Me.vue
+++ src/pages/account/Me.vue
@@ -1,20 +1,42 @@
 <template>
   <ion-page>
     <MainHeader title="My account" />
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Home</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-button id="present-alert" color="danger">Delete my account</ion-button>
+      <ion-alert
+        trigger="present-alert"
+        header="Confirm delete"
+        :buttons="alertButtons"
+      ></ion-alert>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
-  import ExploreContainer from '@/components/ExploreContainer.vue'
+  import { getAuth, deleteUser } from 'firebase/auth'
+
+  import { IonPage, IonContent, IonButton, IonAlert } from '@ionic/vue'
   import MainHeader from '@/components/MainHeader.vue'
+
+  const auth = getAuth()
+  const user = auth.currentUser
+
+  const alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Delete',
+      role: 'destructive',
+      handler: async () => {
+        try {
+          user && (await deleteUser(user))
+        } catch (e) {
+          console.error(e)
+        }
+        window.location.assign('/')
+      },
+    },
+  ]
 </script>
